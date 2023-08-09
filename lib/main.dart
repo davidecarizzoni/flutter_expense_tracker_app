bba8bf4944f0226ae829137dcad269d392189ba9
davidecarizzoni <davide.carizzoni@runelab.it>
2023-08-09 18:27:13 +0200

    feat(init)

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:expense_tracker/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(),
+      home: const Expenses(),
+    ),
+  );
+}
